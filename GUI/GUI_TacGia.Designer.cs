cb1732c7eea8c91de13910684b2b51e07538185c
Tien <tn6609092@gmail.com>
2023-11-09 21:24:01 +0700

    ...

--- GUI/GUI_TacGia.Designer.cs
+++ GUI/GUI_TacGia.Designer.cs
@@ -35,18 +35,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(705, 434);
+            this.label1.Location = new System.Drawing.Point(401, 196);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 32);
+            this.label1.Size = new System.Drawing.Size(87, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Tac Gia";
             // 
             // GUI_TacGia
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1329, 776);
+            this.ClientSize = new System.Drawing.Size(997, 630);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "GUI_TacGia";
             this.Text = "GUI_TacGia";
             this.ResumeLayout(false);
